3f0063696836de1abaa49fbd96655f2bd2486c1a
Eduardo Tolino <eduardo.tolino@tolitech.com.br>
2022-02-17 09:20:32 -0300

    adding PermissionRequirement.

---
+++ src/AspNetCore/Security/PermissionRequirement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Tolitech.CodeGenerator.AspNetCore.Security
+{
+    public class PermissionRequirement : IAuthorizationRequirement
+    {
+        public string Permission { get; set; }
+
+        public PermissionRequirement(string permission)
+        {
+            Permission = permission;
+        }
+    }
+}
